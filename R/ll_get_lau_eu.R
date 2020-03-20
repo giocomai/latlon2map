a85e12e5949f270dee282e2cc859e2279aa62c94
Giorgio Comai <g@giorgiocomai.eu>
2020-03-20 12:45:46 +0100

    introduce caching for lau

--- R/ll_get_lau_eu.R
+++ R/ll_get_lau_eu.R
@@ -1,11 +1,16 @@
 #' Gets local administrative units from Eurostat's website 
 #'
-#' @return LAU in sf format
+#' Source: https://ec.europa.eu/eurostat/web/gisco/geodata/reference-data/administrative-units-statistical-units/lau#lau18
+#'
+#' @param year Year of mapping, defaults to most recent (2018). Availalble values: 2017, 2018
+#'
+#' @return European LAU in sf format
 #' @export
 #'
 #' @examples
 #' ll_get_lau_eu()
-ll_get_lau_eu <- function(year = 2018) {
+#' 
+ll_get_lau_eu <- function(name = NULL, year = 2018) {
   usethis::ui_info(x = "© EuroGeographics for the administrative boundaries")
   ll_create_folders(geo = "eu",
                     level = "lau",
@@ -18,35 +23,59 @@ ll_get_lau_eu <- function(year = 2018) {
                            name = "abl",
                            file_type = "rds")
   
-  if (fs::file_exists(rds_file)) {
-    return(readr::read_rds(rds_file))
+  if (is.null(name)==FALSE) {
+    rds_file_location <- ll_find_file(geo = "eu",
+                                      level = "lau",
+                                      resolution = "1m",
+                                      year = year,
+                                      name = paste0("lau-", name),
+                                      file_type = "rds")
+    
+    if (fs::file_exists(rds_file_location)) {
+      return(readr::read_rds(rds_file_location))
+    }
   }
-  shp_folder <- ll_find_file(geo = "eu",
+  
+  
+  if (fs::file_exists(rds_file)) {
+    sf <- readr::read_rds(rds_file)
+  } else {
+    shp_folder <- ll_find_file(geo = "eu",
+                               level = "lau",
+                               resolution = "1m",
+                               year = year,
+                               name = "abl",
+                               file_type = "shp")
+    
+    source_url <- paste0("https://ec.europa.eu/eurostat/cache/GISCO/geodatafiles/LAU-", year, "-01M-SH.zip")
+    zip_file <- ll_find_file(geo = "eu",
                              level = "lau",
                              resolution = "1m",
                              year = year,
                              name = "abl",
-                             file_type = "shp")
-  
-  source_url <- paste0("https://ec.europa.eu/eurostat/cache/GISCO/geodatafiles/LAU-", year, "-01M-SH.zip")
-  zip_file <- ll_find_file(geo = "eu",
-                           level = "lau",
-                           resolution = "1m",
-                           year = year,
-                           name = "abl",
-                           file_type = "zip")
-  
-  if (fs::file_exists(zip_file)==FALSE) {
-    download.file(url = source_url,
-                  destfile = zip_file)
+                             file_type = "zip")
+    
+    if (fs::file_exists(zip_file)==FALSE) {
+      download.file(url = source_url,
+                    destfile = zip_file)
+    }
+    unzip(zipfile = zip_file,
+          exdir = shp_folder)
+    sf <- sf::read_sf(shp_folder) %>% 
+      dplyr::mutate(CNTR_CODE = stringr::str_extract(string = GISCO_ID,
+                                                     pattern = "[[A-Z]][[A-Z]]")) %>% 
+      sf::st_transform(crs = 4326)
+    readr::write_rds(x = sf,
+                     path = rds_file)
+    
   }
-  unzip(zipfile = zip_file,
-        exdir = shp_folder)
-  sf <- sf::read_sf(shp_folder) %>% 
-    dplyr::mutate(CNTR_CODE = stringr::str_extract(string = GISCO_ID,
-                                                   pattern = "[[A-Z]][[A-Z]]")) %>% 
-    sf::st_transform(crs = 4326)
-  readr::write_rds(x = sf,
-                   path = rds_file)
+  
+  if (is.null(name)==FALSE) {
+    sf <- sf %>% 
+      dplyr::filter(LAU_LABEL == name)
+    readr::write_rds(x = sf,
+                     path = rds_file_location)
+  }
+  
   return(sf)
 }
--- R/ll_get_nuts_it.R
+++ R/ll_get_nuts_it.R
@@ -9,11 +9,27 @@
 #'
 #' @examples
 #' ll_get_nuts_it()
-ll_get_nuts_it <- function(level = 2,
+#' ll_get_nuts_it(name = "Rimini")
+ll_get_nuts_it <- function(name = NULL,
+                           level = 2,
                            year = 2019,
                            resolution = "low") {
   usethis::ui_info(x = "Source: https://www.istat.it/it/archivio/222527")
   usethis::ui_info(x = "Istat (CC-BY)")
+  
+  if (is.null(name)==FALSE) {
+    rds_file_location <- ll_find_file(geo = "it",
+                                      level = level,
+                                      resolution = resolution,
+                                      year = year,
+                                      name = paste0(level, "-", name),
+                                      file_type = "rds")
+    
+    if (fs::file_exists(rds_file_location)) {
+      return(readr::read_rds(rds_file_location))
+    }
+  }
+  
   
   rds_file <- ll_find_file(geo = "it",
                            level = level,
@@ -23,71 +39,92 @@ ll_get_nuts_it <- function(level = 2,
                            file_type = "rds")
   
   if (fs::file_exists(rds_file)) {
-    return(readr::read_rds(path = rds_file))
-  }
-  
-  ll_create_folders(geo = "it",
-                    level = level,
-                    resolution = "low",
-                    year = year)
-  ll_create_folders(geo = "it",
-                    level = "all_levels",
-                    resolution = "low",
-                    year = year)
-  
-  shp_folder <- ll_find_file(geo = "it",
+    sf <- readr::read_rds(path = rds_file)
+  } else {
+    ll_create_folders(geo = "it",
+                      level = level,
+                      resolution = "low",
+                      year = year)
+    ll_create_folders(geo = "it",
+                      level = "all_levels",
+                      resolution = "low",
+                      year = year)
+    
+    shp_folder <- ll_find_file(geo = "it",
+                               level = "all_levels",
+                               resolution = resolution,
+                               year = year,
+                               name = "abl",
+                               file_type = "shp")
+    
+    type <- dplyr::if_else(condition = resolution == "high",
+                           true = "non_generalizzati",
+                           false = "generalizzati",
+                           missing = "non_generalizzati")
+    
+    g_name <- dplyr::if_else(condition = resolution == "high",
+                             true = "",
+                             false = "_g",
+                             missing = "")
+    
+    source_url <- paste0("http://www.istat.it/storage/cartografia/confini_amministrativi/", type, "/Limiti0101", year, g_name, ".zip")
+    
+    zip_file <- ll_find_file(geo = "it",
                              level = "all_levels",
                              resolution = resolution,
                              year = year,
                              name = "abl",
-                             file_type = "shp")
-  
-  type <- dplyr::if_else(condition = resolution == "high",
-                         true = "non_generalizzati",
-                         false = "generalizzati",
-                         missing = "non_generalizzati")
-  
-  g_name <- dplyr::if_else(condition = resolution == "high",
-                           true = "",
-                           false = "_g",
-                           missing = "")
-  
-  source_url <- paste0("http://www.istat.it/storage/cartografia/confini_amministrativi/", type, "/Limiti0101", year, g_name, ".zip")
-  
-  zip_file <- ll_find_file(geo = "it",
-                           level = "all_levels",
-                           resolution = resolution,
-                           year = year,
-                           name = "abl",
-                           file_type = "zip")
-  
-  
-  if (fs::file_exists(zip_file)==FALSE) {
-    download.file(url = source_url, destfile = zip_file)
+                             file_type = "zip")
+    
+    
+    if (fs::file_exists(zip_file)==FALSE) {
+      download.file(url = source_url, destfile = zip_file)
+    }
+    
+    unzip(zipfile = zip_file, exdir = shp_folder)
+    
+    
+    if (level == "lau") {
+      sf <- sf::read_sf(fs::path(shp_folder,
+                                 paste0("Limiti01012019", g_name),
+                                 paste0("Com01012019", g_name)))
+    } else if (level == 1) {
+      sf <- sf::read_sf(fs::path(shp_folder,
+                                 paste0("Limiti01012019", g_name),
+                                 paste0("RipGeo01012019", g_name)))
+    } else if (level == 2) {
+      sf <- sf::read_sf(fs::path(shp_folder,
+                                 paste0("Limiti01012019", g_name),
+                                 paste0("Reg01012019", g_name)))
+    } else if (level == 3) {
+      sf <- sf::read_sf(fs::path(shp_folder,
+                                 paste0("Limiti01012019", g_name),
+                                 paste0("ProvCM01012019", g_name)))
+    }
+    sf <- sf %>% 
+      sf::st_transform(crs = 4326)
+    
+    readr::write_rds(x = sf, path = rds_file)
+    
   }
   
-  unzip(zipfile = zip_file, exdir = shp_folder)
-  
-  
-  if (level == "lau") {
-    sf <- sf::read_sf(fs::path(shp_folder,
-                               paste0("Limiti01012019", g_name),
-                               paste0("Com01012019", g_name)))
-  } else if (level == 1) {
-    sf <- sf::read_sf(fs::path(shp_folder,
-                               paste0("Limiti01012019", g_name),
-                               paste0("RipGeo01012019", g_name)))
-  } else if (level == 2) {
-    sf <- sf::read_sf(fs::path(shp_folder,
-                               paste0("Limiti01012019", g_name),
-                               paste0("Reg01012019", g_name)))
-  } else if (level == 3) {
-    sf <- sf::read_sf(fs::path(shp_folder,
-                               paste0("Limiti01012019", g_name),
-                               paste0("ProvCM01012019", g_name)))
+  if (is.null(name)==FALSE) {
+    if (level == "lau") {
+      sf <- sf %>% 
+        dplyr::filter(COMUNE == name)
+    } else if (level == 1) {
+      sf <- sf %>% 
+        dplyr::filter(DEN_RIP == name)
+    } else if (level == 2) {
+      sf <- sf %>% 
+        dplyr::filter(DEN_REG == name)
+    } else if (level == 3) {
+      sf <- sf %>% 
+        dplyr::filter(DEN_PROV == name)
+    }
+    
+    readr::write_rds(x = sf,
+                     path = rds_file_location)
   }
-  readr::write_rds(x = sf %>% 
-                     sf::st_transform(crs = 4326), path = rds_file)
-  return(sf %>% 
-           sf::st_transform(crs = 4326))
+  return(sf)
 }
