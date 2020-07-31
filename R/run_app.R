893658d3cd4db22a796cafc49921f283461c3bf7
Giorgio Comai <g@giorgiocomai.eu>
2020-07-31 09:59:35 +0200

    add new param to set cache folder when launching shiny app

--- R/run_app.R
+++ R/run_app.R
@@ -2,10 +2,12 @@
 #'
 #' @param ... A series of options to be used inside the app.
 #' @param max_file_size Maximum file size to accept for upload expressed in MB, defaults to 100.
+#' @param ll_folder_path If given, sets the folder to use for caching, corresponds to `ll_set_folder()`. Useful e.g. for Docker deployments. Defaults to NULL.
 #' @export
 #' @importFrom shiny shinyApp
 #' @importFrom golem with_golem_options
 ll_app <- function(max_file_size = 100,
+                   ll_folder_path = NULL,
   ...
 ) {
   with_golem_options(
@@ -13,6 +15,8 @@ ll_app <- function(max_file_size = 100,
       ui = app_ui, 
       server = app_server
     ), 
-    golem_opts = list(shiny.maxRequestSize=max_file_size*1024^2, ...)
+    golem_opts = list(shiny.maxRequestSize=max_file_size*1024^2,
+                      ll_folder_path = ll_folder_path,
+                      ...)
   )
 }
