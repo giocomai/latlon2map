34b200899260d9bc474656c838f68067b48d3f0b
Giorgio Comai <g@giorgiocomai.eu>
2020-08-07 13:02:33 +0200

    introduce option to scale the label size via xml2

--- R/ll_export_sf_to_kml.R
+++ R/ll_export_sf_to_kml.R
@@ -2,6 +2,8 @@
 #'
 #' Attention: this function requires libkml. 
 #' 
+#' Attention: label styling is not currently functional, likely due to issues in passing arguments to libkml. In order to change label size, use label_scale, which directly edits the xml file. 
+#' 
 #' For further details on the exact meaning of each of the parameters, please consult the documentation of OGR (used by GDAL to pass parameters to .kml): https://gdal.org/user/ogr_feature_style.html
 #'
 #' @param sf An object of class `sf`
@@ -12,6 +14,7 @@
 #' @param label_text Column to be used as label text. Defaults to NULL. Corresponds to "LABEL" element in OGR.
 #' @param label_font Font family to be used for the font. Defaults to "Roboto Sans, Noto Sans, Helvetica"
 #' @param label_size Size of the label. Defaults to "24pt"
+#' @param label_scale Scale of label. Defaults to NULL. If given, changes label size (e.g. 1 = default, 2 = twice as big, 0.5, half as big, etc.)
 #' @param label_placement Defaults to "m" (centre and middle-aligned). For more options, check: https://gdal.org/user/ogr_feature_style.html
 #' @param icon_url Defaults to "" for no URL. Corresponds to "SYMBOL" in OGR. In case of wrong inputs, Google Earth may show you an ugly yellow pushpin instead (i.e. default to http://maps.google.com/mapfiles/kml/pushpin/ylw-pushpin.png). Available icons offered by Google available at this link: http://kml4earth.appspot.com/icons.html
 #' @param icon_colour Defaults to "#000000ff" (i.e. black, with 100% opacity).
@@ -32,6 +35,7 @@ ll_export_sf_to_kml <- function(sf,
                                 label_font = "Roboto Sans, Noto Sans, Helvetica",
                                 label_size = "24pt",
                                 label_placement = "m",
+                                label_scale = NULL,
                                 line_colour = "#ffffffff", 
                                 line_width = "3px",
                                 icon_url = "",
@@ -72,4 +76,30 @@ ll_export_sf_to_kml <- function(sf,
   sf::st_write(obj = sf_pre_kml,
                dsn = path,
                driver = "libkml")
+  
+  if (is.null(label_scale)==FALSE) {
+    xml_list <- xml2::read_xml(x = path) %>% 
+      xml2::as_list()
+    id_of_placemarks <- seq_along(purrr::pluck(xml_list,"kml", "Document", "Document"))[-1]
+    
+    for (i in id_of_placemarks) {
+      if (is.element("Style", names(xml_list[["kml"]][["Document"]][["Document"]][[i]]))==FALSE) {
+        new_item_location <- length(xml_list[["kml"]][["Document"]][["Document"]][[i]])+1
+        xml_list[["kml"]][["Document"]][["Document"]][[i]][[new_item_location]] <- "Style"
+      }
+      
+      if (is.element("LabelStyle", names(xml_list[["kml"]][["Document"]][["Document"]][[i]][["Style"]]))==FALSE) {
+        new_item_location <- length(xml_list[["kml"]][["Document"]][["Document"]][[i]][["Style"]])+1
+        xml_list[["kml"]][["Document"]][["Document"]][[i]][["Style"]][[new_item_location]] <- "LabelStyle"
+      }
+      
+      
+      new_item_location <- length(xml_list[["kml"]][["Document"]][["Document"]][[i]][["Style"]][["LabelStyle"]])+1
+      xml_list[["kml"]][["Document"]][["Document"]][[i]][["Style"]][["LabelStyle"]][[new_item_location]] <- "scale"
+      xml_list[["kml"]][["Document"]][["Document"]][[i]][["Style"]][["LabelStyle"]][["scale"]][[1]] <- label_scale
+    }
+    xml_list %>% 
+      xml2::as_xml_document() %>% 
+      xml2::write_xml(file = path)
+  }
 }
