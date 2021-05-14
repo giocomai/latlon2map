eb545fe7d128ff34394288daf973f1508b68918b
Giorgio Comai <g@giorgiocomai.eu>
2021-05-15 01:03:26 +0200

    move to more recent module practice

--- R/mod_file_input.R
+++ R/mod_file_input.R
@@ -18,28 +18,31 @@ mod_file_input_ui <- function(id, label = "CSV file"){
 #' file_input Server Function
 #'
 #' @noRd 
-mod_file_input_server <- function(input, output, session){
-  ns <- session$ns
-  user_file <- reactive({
-    # If no file is selected, don't do anything
-    validate(need(input$file_input, message = FALSE))
-    input$file_input
-  })
+mod_file_input_server <- function(id){
   
-  df <- reactive({
-    if (fs::path_ext(user_file()$name)=="csv") {
-      readr::read_csv(file = user_file()$datapath)
-    } else if (fs::path_ext(user_file()$name)=="tsv"){
-      readr::read_tsv(file = user_file()$datapath)
-    } else if (fs::path_ext(user_file()$name)=="xslx"|fs::path_ext(user_file()$name)=="xsl") {
-      readxl::read_excel(path = user_file()$datapath)
-    } else if (fs::path_ext(user_file()$name)=="ods") {
-      readODS::read_ods(path = user_file()$datapath)
-    }
+  moduleServer(id, function(input, output, session){
+    ns <- session$ns
+    user_file <- reactive({
+      # If no file is selected, don't do anything
+      validate(need(input$file_input, message = FALSE))
+      input$file_input
+    })
     
+    df <- reactive({
+      if (fs::path_ext(user_file()$name)=="csv") {
+        readr::read_csv(file = user_file()$datapath)
+      } else if (fs::path_ext(user_file()$name)=="tsv"){
+        readr::read_tsv(file = user_file()$datapath)
+      } else if (fs::path_ext(user_file()$name)=="xslx"|fs::path_ext(user_file()$name)=="xsl") {
+        readxl::read_excel(path = user_file()$datapath)
+      } else if (fs::path_ext(user_file()$name)=="ods") {
+        readODS::read_ods(path = user_file()$datapath)
+      }
+      
+    })
+    
+    return(df)
   })
-  
-  return(df)
 }
     
 ## To be copied in the UI
