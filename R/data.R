3f7d4c84e877d63e59ea0912085ac45d1999ff69
Giorgio Comai <g@giorgiocomai.eu>
2021-09-21 10:14:28 +0200

    add full dataset of high resolution pop grids to be integrated in ll_get_population_grid_hr()

--- R/data.R
+++ R/data.R
@@ -25,16 +25,22 @@
 #' A dataset with all names of geographic entities available for direct download as geopackage files
 #' They are used to download files with `ll_osm_download_it()`
 #'
-#' Links to shapefiles are stored as tibbles. Unnest to see them, e.g.
-#' `ll_osm_countries %>% tidyr::unnest(link)`
-#' or for a single country:
-#' `ll_osm_countries %>% dplyr::filter(country == "italy") %>% tidyr::unnest(link)`
+#' @format A list of tibbles
+#' @source \url{https://osmit-estratti.wmcloud.org/}
+"ll_osm_it_gpkg"
+
+
+#' A data frame with links to High Resolution Population Density Maps distributed by Facebook on HDX
+#'
+#' It is used to download files with `ll_get_population_grid_hr()`
+#'
 #'
 #' @format A tibble
 #' \describe{
-#'   \item{continent}{Name of the continent}
-#'   \item{country}{Name of the country}
-#'   \item{link}{Link to shapefiles in a tibble}
+#'   \item{country}{Name of the country in English}
+#'   \item{country_code}{Two letter code as used by eurostat, see also `countrycode::codelist$eurostat`}
+#'   \item{download_ulr}{Link to zipped dataset}
+#'   \item{url}{Link to page describing the dataset}
 #' }
-#' @source \url{http://download.geofabrik.de/}
-"ll_osm_countries"
+#' @source \url{https://data.humdata.org/}
+"population_grid_hr_metadata"
---
+++ data-raw/population_grid_hr_links.R
@@ -0,0 +1,88 @@
+## code to prepare `population_grid_hr_links` dataset goes here
+
+# remotes::install_gitlab("dickoa/rhdx")
+# https://gitlab.com/dickoa/rhdx
+library("rhdx")
+set_rhdx_config(hdx_site = "prod")
+get_rhdx_config()
+
+library("tidyverse")
+hr <- search_datasets(query = "High Resolution Population Density Maps",
+                      rows = 1000)
+
+
+pb <- progress::progress_bar$new(total = length(hr))
+
+all_available_df <- purrr::map_dfr(.x = hr,
+                                   .f = function(x) {
+  pb$tick()
+  current_resources <- x %>% get_resources()  
+
+ # pb <- progress::progress_bar$new(total = length(current_resources))
+  purrr::map_dfr(.x = current_resources,
+                 .f = function(current_resource)
+                 {
+                   #                 pb$tick()
+                   resource_list <- current_resource$as_list() 
+                   
+                   resource_list[names(resource_list)==""] <- NULL
+    
+                   resource_df <- resource_list %>%
+                     tibble::enframe() %>% 
+                     tidyr::pivot_wider() %>% 
+                     tidyr::unnest(cols = dplyr::everything()) %>% 
+                     mutate(across(everything(), as.character))
+                   
+                   # if (is.element("originalHash", names(resource_df))) {
+                   #   resource_df$originalHash <- as.character(resource_df$originalHash)
+                   # }
+                   # if (is.element("pii", names(resource_df))) {
+                   #   resource_df$pii <- as.character(resource_df$pii)
+                   # }
+                   resource_df 
+                 })  %>% 
+    dplyr::mutate(
+      title =  x$as_list()$title,
+      country = list(x$as_list()$solr_additions %>% jsonlite::parse_json() %>% unlist()),
+      dataset_source = x$as_list()$dataset_source,
+      dataset_name = x$as_list()[["name"]]
+                  )
+  
+})
+
+
+population_grid_hr_metadata <- all_available_df %>% 
+  dplyr::filter(dataset_source == "Facebook") %>% 
+  dplyr::filter(url_type == "upload") %>% 
+  dplyr::select(package_id,
+                id,
+                size,
+                metadata_modified, 
+                download_url,
+                format,
+                position,
+                name,
+                created,
+                last_modified,
+                title, 
+                country, 
+                dataset_name) %>% 
+  tidyr::unnest(country) %>% 
+  dplyr::mutate(country_code = countrycode::countrycode(sourcevar = country,
+                         origin = "country.name.en",
+                         destination = "eurostat")) %>% 
+  dplyr::mutate(url = stringr::str_c("https://data.humdata.org/dataset/", dataset_name)) %>% 
+  dplyr::select(country_code,
+                country,
+                format,
+                download_url,
+                name,title,
+                dplyr::everything()) %>% 
+  dplyr::group_by(country, name) %>% 
+  dplyr::slice_max(last_modified) %>% 
+  dplyr::ungroup() %>% 
+  dplyr::arrange(country_code, country, format, name)  
+
+  
+usethis::use_data(population_grid_hr_metadata,
+                  overwrite = TRUE)
