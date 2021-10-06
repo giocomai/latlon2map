50f70d69162c8c559197689ba87685b4e927bb24
Giorgio Comai <g@giorgiocomai.eu>
2021-10-06 20:22:13 +0200

    use primarily nuts_id for more consistency in results

--- R/ll_get_nuts_eu.R
+++ R/ll_get_nuts_eu.R
@@ -2,20 +2,26 @@
 #'
 #' Source: https://ec.europa.eu/eurostat/web/gisco/geodata/reference-data/administrative-units-statistical-units/nuts#nuts16
 #'
+#' @param nuts_id NUTS id. Must correspond to the level given. e.g. "DE149" for the NUTS3 regionion of Sigmaringen in Germany.
+#' @param nuts_name Name of the NUTS region. Run `ll_get_nuts_eu()` to check valid values in `NUTS_NAME` column.
 #' @param level Defaults to 3, corresponding to nuts3. Available values are: 0, 1, 2, and 3.
-#' @param resolution Defaults to "60", for 1:60 Million. Available values: are 20, 10, 3, 1 (1 is highest quality available)-
+#' @param resolution Defaults to "60", for 1:60 Million. Available values: are 20, 10, 3, 1 (1 is highest quality available).
 #' @param year Defaults to 2021 Available values: 2021, 2016, 2013, 2010, 2006, 2003
 #' @return NUTS in sf format
 #' @export
 #'
 #' @examples
-#' ll_get_lau_eu()
-ll_get_nuts_eu <- function(name = NULL,
+#' ll_get_nuts_eu()
+ll_get_nuts_eu <- function(nuts_id = NULL,
+                           nuts_name = NULL,
                            level = 3,
                            resolution = 60,
                            year = 2021,
                            silent = FALSE) {
-  resolution <- stringr::str_pad(string = resolution, width = 2, side = "left", pad = 0)
+  resolution <- stringr::str_pad(string = resolution,
+                                 width = 2,
+                                 side = "left",
+                                 pad = 0)
 
   if (silent == FALSE) {
     usethis::ui_info("© EuroGeographics for the administrative boundaries")
@@ -45,7 +51,21 @@ ll_get_nuts_eu <- function(name = NULL,
     file_type = "rds"
   )
 
-  if (is.null(name) == FALSE) {
+  if (is.null(nuts_id)==FALSE) {
+    
+    rds_file_location <- ll_find_file(
+      geo = "eu",
+      level = level,
+      resolution = resolution,
+      year = year,
+      name = nuts_id,
+      file_type = "rds"
+    )
+    
+    if (fs::file_exists(rds_file_location)) {
+      return(readr::read_rds(file = rds_file_location))
+    }
+  } else if (is.null(name) == FALSE) {
     rds_file_location <- ll_find_file(
       geo = "eu",
       level = level,
@@ -101,18 +121,24 @@ ll_get_nuts_eu <- function(name = NULL,
       )
     }
     sf <- sf::read_sf(fs::path(shp_folder, paste0("4326-nuts", level)))
-    readr::write_rds(
-      x = sf,
+    saveRDS(
+      object = sf,
       file = rds_file
     )
   }
-  if (is.null(name) == FALSE) {
+  
+  if (is.null(nuts_id)==FALSE) {
     sf <- sf %>%
-      dplyr::filter(NUTS_NAME == name)
-    readr::write_rds(
-      x = sf,
-      file = rds_file_location
-    )
+      dplyr::filter(NUTS_ID == nuts_id)
+    
+    saveRDS(object = sf,
+            file = rds_file_location)
+  } else if (is.null(name) == FALSE) {
+    sf <- sf %>%
+      dplyr::filter(NUTS_NAME == nuts_name)
+    
+    saveRDS(object = sf,
+            file = rds_file_location)
   }
   return(sf)
 }
