a3237866c13b2a56fff30a1f28506dbb993b3481
Giorgio Comai <g@giorgiocomai.eu>
2026-03-14 23:24:18 +0100

    integrate documentation, dontrun example

--- R/ll_find_pop_centre.R
+++ R/ll_find_pop_centre.R
@@ -1,35 +1,45 @@
-#' Find the population-weighted centre of a municipality
+#' Find the population-weighted centre of a municipality or other geographic
+#' entity
 #'
-#' @param sf_location
-#' @param sf_population_grid
-#' @param power Defaults to 2. To give more weight to cells with higher population density, raise the number of residents by the power of.
-#' @param join Defaults to sf::st_intersects.
-#' @param adjusted If adjusted is set to TRUE, join is ignored. The population of cells along the boundary line are weighted by the share of the cell included within the border.
+#' @param sf_location Relevant gepgraphic entity as an  `sf` object, typically
+#'   retrieved with [ll_get_lau_eu()] or similar.
+#' @param sf_population_grid Relevant population grid, typically retrieved with
+#'   [ll_get_population_grid()].
+#' @param power Defaults to 2. To give more weight to cells with higher
+#'   population density, raise the number of residents by the power of.
+#' @param join Defaults to [sf::st_intersects()].
+#' @param adjusted If adjusted is set to `TRUE`, join is ignored. The population
+#'   of cells along the boundary line are weighted by the share of the cell
+#'   included within the border.
 #'
-#' @return
+#' @return A point as an `sf` object. The population-weighted centre for the
+#'   relevant entity.
 #' @export
 #'
 #' @examples
+#' \dontrun{
+#'   ll_set_folder("~/R/")
+#'   name <- "Pinzolo"
+#'   sf_location <- ll_get_nuts_it(name = name,
+#'                                 level = "lau",
+#'                                 resolution = "high")
 #'
-#' ll_set_folder("~/R/")
-#' name <- "Pinzolo"
-#' sf_location <- ll_get_nuts_it(name = name, level = "lau", resolution = "high")
+#'   lau_grid_name_temp <- stringr::str_c(name, "_lau_high-st_intersects")
 #'
-#' lau_grid_name_temp <- stringr::str_c(name, "_lau_high-st_intersects")
-#'
-#' sf_location_grid <- ll_get_population_grid(
-#'   match_sf = sf_location,
-#'   match_name = lau_grid_name_temp,
-#'   match_country = "IT",
-#'   join = sf::st_intersects
-#' )
+#'   sf_location_grid <- ll_get_population_grid(
+#'     match_sf = sf_location,
+#'     match_name = lau_grid_name_temp,
+#'     match_country = "IT",
+#'     join = sf::st_intersects
+#'   )
 #'
 #'
-#' pop_centre <- ll_find_pop_centre(
-#'   sf_location = sf_location,
-#'   sf_population_grid = sf_location_grid,
-#'   power = 2
-#' )
+#'   pop_centre <- ll_find_pop_centre(
+#'     sf_location = sf_location,
+#'     sf_population_grid = sf_location_grid,
+#'     power = 2
+#'   )
+#' }
 ll_find_pop_centre <- function(
   sf_location,
   sf_population_grid,
